b4bfc1aabec9aa17673b5846e671dbbf8ec8aec8
jiaxiu11 <jiaxiunov@gmail.com>
2018-10-28 10:23:30 +0800

    Merge branch 'master' of https://github.com/jiaxiu11/1564api

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -91,3 +91,72 @@ export const deleteNote = (req, res) => {
     });
 });
 };
+
+
+
+
+/*
+i'll save the orginal code here
+
+export const Note = mongoose.model('Note', noteSchema);
+export const getCardWithId = (req, res) => {
+  Note.findById(req.params.noteId, (error, parentNote) => {
+    if (error) {
+      res.send(err);
+    }
+    var card = parentNote.cards.id(req.params.cardId);
+    res.json(card);
+  });
+};//i'll just leave it here first, might not use it
+
+
+export const addNewNote = (req, res) => {
+  let newNote = new Note(req.body);
+
+  newNote.save((err, note) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(note);
+  });
+};
+
+export const getNotes = (req, res) => {
+  Note.find({}, (err, notes) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(notes);
+  });
+};
+
+export const getNoteWithId = (req, res) => {
+  Note.findById(req.params.noteId, (error, note) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(note);
+  });
+};//i'll just leave it here first, might not use it
+
+export const updateNote = (req, res) => {
+  //console.log(typeof req.params.noteId)
+  Note.findOneAndUpdate({_id : req.params.noteId},//or {_id: req.params.noteId}
+req.body, {new : true}, (err, note) => { //the new option tells it to return the updated one
+  if (err) {
+    res.send(err);
+  }
+  res.json(note);
+});
+};
+
+export const deleteNote = (req, res) => {
+  Note.remove({_id: req.params.noteId}, (err, note) => {
+  if (err) {
+    res.send(err);
+  }
+  res.json({message: 'delete successful'});
+});
+};
+
+*/
